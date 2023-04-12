91a13deda2fb7116632a95fa8a31cbcf60f16706
Pinto Manuel <pinto@fireworkx.co.za>
2023-04-13 01:23:53 +0200

    Commit code

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -3,11 +3,12 @@ import Jumbotron from '../components/specific/Jumbotron'
 import image from '../assets/images/banner.jpg'
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Grid, Paper, Typography } from '@mui/material';
+
 
 const Account = () => {
   const navigate = useNavigate();
   const user = useSelector((state) => state.user || {});
-
   console.log("Account User", user);
 
 
@@ -28,6 +29,44 @@ const Account = () => {
         buttonLink="/about"
         image={image}
       />
+
+      <Grid item xs={12} sm={6} md={4}
+        sx={{
+          display: 'flex',
+          justifyContent: 'start',
+
+        }}
+      >
+        <Paper elevation={5} sx={{
+          width: '100%',
+          padding: '10px',
+          margin: '40px',
+          textAlign: 'center'
+        }}>
+          <Typography variant="h5"
+            style={{ marginBottom: '10px' }}>
+            Account Details:
+          </Typography>
+          <Grid container spacing={2}
+            style={{
+              display: "flex",
+              flexDirection: 'column',
+              alignItems: 'center'
+            }}>
+            <Grid item xs={12} md={6}>
+              <Typography variant="subtitle1">
+                Email: {user.email}
+              </Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="subtitle1">
+                Password: {user.password}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+
     </div>
   )
 }
